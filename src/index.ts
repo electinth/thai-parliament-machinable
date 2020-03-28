9bc3e7cc17011a06d2cc84d8687a3fcc49fc0f4b
mixth <me@mixth.net>
2020-03-28 19:38:05 +0700

    Export file as JSON

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import * as lis from './parliament-lis';
 import * as https from 'https';
+import fs from 'fs';
 
-const option: lis.Option = {
-  sapaNo: 25,
-  system: lis.System.Motion,
-  toPage: 1,
-  limiter: {
-    maxConcurrent: 1,
-    minTime: 0,
-  }
+const exportToJson = async (): Promise<void> => {
+  const option: lis.Option = {
+    sapaNo: 25,
+    system: lis.System.Motion,
+    fromPage: 1,
+    toPage: 1,
+    limiter: {
+      maxConcurrent: 1,
+      minTime: 0,
+    }
+  };
+  
+  const agent = new https.Agent({
+    minVersion: 'TLSv1',
+  });
+  
+  const motions = await lis.getAll(option, agent);
+  return fs.writeFileSync('./results/motion.json', JSON.stringify(motions, null, 4));
 };
 
-const agent = new https.Agent({
-  minVersion: 'TLSv1',
-});
-
-lis.getAll(option, agent);
+exportToJson();
