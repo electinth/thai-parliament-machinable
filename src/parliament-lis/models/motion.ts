4bbc2f67e9613e8e1794c11190e572247ae0b258
mixth <me@mixth.net>
2020-03-28 22:03:30 +0700

    Parse status of motion with some samples

--- src/parliament-lis/models/motion.ts
+++ src/parliament-lis/models/motion.ts
@@ -16,12 +16,33 @@ export class Motion {
 }
 
 export const parseStatus = (text: string): Status => {
-  return Status.Unknown;
+  if (!(text in statusMap)) return Status.Unknown;
+  return statusMap[text];
 };
 
 export enum Status {
-  Unknown
+  Processing = 'อยู่ระหว่างการดำเนินการของเจ้าหน้าที่',
+  InQueue = 'รอบรรจุระเบียบวาระเพื่อพิจารณา',
+  InSession = 'อยู่ระหว่างการพิจารณา',
+  SentToCabinet = 'ส่งคณะรัฐมนตรีดำเนินการแล้ว',
+  ParliamentAppointedCommittee = 'มีการแต่งตั้งคณะกรรมาธิการ',
+  CommitteeWorking = 'อยู่ระหว่างการพิจารณาของคณะกรรมาธิการ',
+  ParliamentApprovedObservation = 'การดำเนินงานตามข้อสังเกตของคณะกรรมาธิการสภาผู้แทนราษฎร',
+  Unknown = '',
 }
+
+const statusMap: {[ id: string ]: Status } = {
+  'อยู่ระหว่างการดำเนินการของเจ้าหน้าที่': Status.Processing,
+  'รอบรรจุระเบียบวาระเพื่อพิจารณา': Status.InQueue,
+  'อยู่ระหว่างการพิจารณา': Status.InSession,
+  'หนังสือส่งค.ร.ม.เรียบร้อยแล้ว': Status.SentToCabinet,
+  'ส่งคณะรัฐมนตรีดำเนินการแล้ว': Status.SentToCabinet,
+  'ส่งให้คณะรัฐมนตรี': Status.SentToCabinet,
+  'มีการแต่งตั้งคณะกรรมาธิการ': Status.ParliamentAppointedCommittee,
+  'อยู่ระหว่างการพิจารณาของคณะกรรมาธิการ': Status.CommitteeWorking,
+  'การดำเนินงานตามข้อสังเกตของคณะกรรมาธิการสภาผู้แทนราษฎร': Status.ParliamentApprovedObservation,
+  'ผลการดำเนินงานตามข้อสังเกตของคณะกรรมาธิการ': Status.ParliamentApprovedObservation,
+};
 
 export class VotedResult {
 
